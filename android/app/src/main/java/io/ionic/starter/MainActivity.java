558b5fd02f949aca38a6ddab9fac24ad6105fa64
Keyvin Diaz <keyvin.95@gmail.com>
2022-12-18 15:11:49 -0400

    sync android platform

--- android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -1,5 +1,13 @@
 package io.ionic.starter;
 
+import android.os.Bundled;
+import com.codetrixstudio.capacitor.GoogleAuth.GoogleAuth;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+  public void onCreate(Bundled savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    registerPlugin(GoogleAuth.class);
+  }
+}
