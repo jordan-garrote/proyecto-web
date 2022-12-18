9a691898124b83af3c8f71e2dc28075d8a5440cb
Keyvin Diaz <keyvin.95@gmail.com>
2022-12-18 12:05:45 -0400

    add @codetrix-studio/capacitor-google-auth

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,14 @@ const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'PadelFixtureUser',
   webDir: 'www',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+  plugins: {
+    'GoogleAuth': {
+      'scopes': ['profile', 'email'],
+      'serverCliendId': '1006375555592-7m7u91rjfivks60j80oo4jg0o89hng33.apps.googleusercontent.com',
+      'forceCodeForRefreshToken': true
+    }
+  }
 };
 
 export default config;
--- src/app/pages/login/login/login.page.ts
+++ src/app/pages/login/login/login.page.ts
@@ -1,36 +1,52 @@
-import { NavController } from '@ionic/angular';
-import { HttpService } from './../../../core/services/http/http.service';
-import { Component, OnInit } from '@angular/core';
+import { NavController, isPlatform } from '@ionic/angular'
+import { HttpService } from './../../../core/services/http/http.service'
+import { Component, OnInit } from '@angular/core'
+import { GoogleAuth } from '@codetrix-studio/capacitor-google-auth'
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+  styleUrls: ['./login.page.scss']
 })
 export class LoginPage implements OnInit {
-
   // email_usuario:string="";    ->cree un objeto para esto
   // contrasena_usuario:string="";
 
   usuario = {
-    email_usuario:"",
-    contrasena_usuario:""
+    email_usuario: '',
+    contrasena_usuario: ''
   }
 
+  user: any = null
 
-  constructor(public navCtrl:NavController,
-    private httpService: HttpService) { }
-
-  ngOnInit() {
+  constructor(
+    public navCtrl: NavController,
+    private httpService: HttpService
+  ) {
+    if (!isPlatform('capacitor')) {
+      GoogleAuth.initialize()
+    }
   }
 
+  ngOnInit() {}
 
-  async btnLogin(mail:string)
-  {
+  async singIn() {
+    this.user = await GoogleAuth.signIn()
+    console.log('user', this.user)
+  }
+
+  async refresh() {
+    const authCode: any = await GoogleAuth.refresh()
+    console.log('refresh', authCode)
+    // { accessToken: 'XXX', idToken: 'XXX' }
+  }
+  async singOut() {
+    this.user = await GoogleAuth.signOut()
+    this.user = null
+  }
+
+  async btnLogin(mail: string) {
     console.log(mail)
-    this.navCtrl.navigateForward('/menu-principal/'+mail);
-
+    this.navCtrl.navigateForward('/menu-principal/' + mail)
   }
-
-
 }
